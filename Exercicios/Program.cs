2f3628aa8447f935622299ae3fc7273643da549b
Caslu-Parra <lucasparra2@outlook.com>
2024-09-13 11:54:13 -0300

    feat: implementa exercicio 02 (Fibonacci)

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -7,15 +7,27 @@
             Console.WriteLine("#### Teste técnico realizado por Lucas Parra - lucasparra2@outlook.com ####");
             Console.WriteLine("## Para inicarmos, selecione o exercicio desejado:");
 
-            Console.WriteLine("- Exercicio 01 (Loop)");
+            Console.WriteLine("- Exercicio 01 (Loop)" + Environment.NewLine +
+                              "- Exercicio 02 (Fibonacci)" + Environment.NewLine);
 
-            Console.Write(Environment.NewLine + "Exercicio selecionado: ");
+            Console.Write("Exercicio selecionado: ");
             switch (Console.ReadLine())
             {
                 case "01":
                 case "1":
+                    Console.Clear();
                     Console.WriteLine(Environment.NewLine + Exercicio01());
                     break;
+               
+                case "02":
+                case "2":
+                    Console.Clear();
+                    Console.Write("# Informe um número: ");
+                    
+                    int num = Convert.ToInt32(Console.ReadLine() ?? "0");
+                    Console.WriteLine(Exercicio02(num) ? $"O número {num} pertence à sequência de Fibonacci." : $"O número {num} NÃO pertence à sequência de Fibonacci.");
+                    break;
+ 
                 default:
                     Console.WriteLine("Exercicio não encontrado...");
                     break;
@@ -30,6 +42,28 @@
                 soma += k;
             }
             return $"# Resultado de soma: {soma}";
+        }
+        private static bool Exercicio02(int num)
+        {
+            bool pertenceAoFibonacci;
+
+            int a = 0, b = 1;
+
+            // Se o número for 0 ou 1, ele pertence à sequência.
+            if (num == 0 || num == 1) pertenceAoFibonacci = true;
+            else
+            {
+                // Gera a sequência até encontrar um número maior ou igual ao informado
+                while (b < num)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+                // Verifica se o número encontrado é igual ao informado
+                pertenceAoFibonacci = b == num;
+            }
+            return pertenceAoFibonacci;
         }
     }
 }
