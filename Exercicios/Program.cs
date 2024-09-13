a9e6c5ea3df2d564e2a91e7d67ddeae9e93e8ebd
Caslu-Parra <lucasparra2@outlook.com>
2024-09-13 11:22:08 -0300

    docs: carga inicial da solução

---
+++ Exercicios/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#### Teste técnico realizado por Lucas Parra - lucasparra2@outlook.com ####");
+        }
+    }
+}
